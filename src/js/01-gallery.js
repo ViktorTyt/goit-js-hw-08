498967a21ef222999c1d2396749ac2f8d2aeb711
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-05-07 00:34:39 +0300

    made 1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const createGallery = args => {
+  const markup = [];
+
+  const item = [...args]
+    .map(
+      arg =>
+        `<a class="gallery__item" href="${arg.original}">
+        <img class= "gallery__image" 
+        src = "${arg.preview}"
+        alt = "${arg.description}"/>
+    </a>`,
+    )
+    .join('');
+
+  markup.push(item);
+
+  refs.gallery.insertAdjacentHTML('afterbegin', markup);
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    fadeSpeed: 250,
+    overlayOpacity: 0.7,
+    captionsData: 'alt',
+  });
+
+  gallery.on('show.simplelightbox', function (e) {
+    // do something…
+  });
+};
+
+createGallery(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+//====Vimeo=======
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const onPlayer = data =>
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+
+player.on('timeupdate', throttle(onPlayer, 1000));
+
+const contPlayer = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(contPlayer);
