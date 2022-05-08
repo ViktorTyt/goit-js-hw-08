9f5f476e1f2233def3088ad4d73c48cd3d169dee
ViktorTyt <92685344+ViktorTyt@users.noreply.github.com>
2022-05-08 08:51:40 +0000

    Deploying to gh-pages from @ ViktorTyt/goit-js-hw-08@083bf63d3092f7cee3f78a0770dcffaca6da79c1 🚀

--- 02-video.21480cf1.js
+++ 02-video.21480cf1.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),u=new e.default(o),a=e=>localStorage.setItem("videoplayer-current-time",JSON.stringify(e.seconds));u.on("timeupdate",(0,t.default)(a,1e3));const i=localStorage.getItem("videoplayer-current-time");u.setCurrentTime(i);
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.c36ba5d8.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.21480cf1.js.map
